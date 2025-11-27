8fe9065751d2691a82e5b6e2dbf29547f4cb1ac8
AbduAlrimali <AbduAlrimali@yahoo.com>
2025-11-27 16:12:13 +0200

    added response models

---
+++ backend/app/schemas/response_models.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel
+
+class LoginSuccess(BaseModel):
+    access_token: str
+    expires_in: int
+    refresh_token: str
+    refresh_expires_in: int
+    token_type: str
+
+    class Config:
+        extra = "allow"
+        json_schema_extra = {
+            "example": {
+                "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9...",
+                "expires_in": 3600,
+                "refresh_token": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9...",
+                "refresh_expires_in": 7200,
+                "token_type": "Bearer"
+            }
+        }
+
+class LoginError(BaseModel):
+    message: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "Example error message during login",
+            }
+        }
+
+class SignupSuccess(BaseModel):
+    message: str
+    user_id: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "User registered successfully",
+                "user_id": "123e4567-e89b-12d3-a456-426614174000",
+            }
+        }
+
+class SignupError(BaseModel):
+    message: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "Example error message during signup",
+            }
+        }
